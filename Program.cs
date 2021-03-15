1916ef2d942c7ed0de4ca5f174566462189748f6
Rafaelspadoni <rafaspadonisolid16@hotmail.com>
2021-03-15 12:08:38 -0300

    revisao 1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = obterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = obterOpcaoUsuario();
+            }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("3- calcularmedia geral");
+            Console.WriteLine("X- sair");
+
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
